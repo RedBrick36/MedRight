d52e943bc27e70ab55ed041d8e0c4665d2fc4332
Roger Baran <roger.baran@icloud.com>
2015-07-01 08:45:25 -0500

    Pre-planning additions

--- src/main/java/org/redbrick/medright/MedRight.java
+++ src/main/java/org/redbrick/medright/MedRight.java
@@ -6,13 +6,34 @@
 
 package org.redbrick.medright;
 
+
 public class MedRight {
+
+/**
+
+ Believe that what I really need here is an array of arrays and not individual arrays
+/* TODO Find out how to create an array of array's
+
+ public string[] treatments;
+     public string[] treatmentName;
+     public double[] doseInMilligrams;
+     public double[] doseInOunces;
+     public double[] milligramsPerTablet;
+     public double[] numberOfOuncesPerTreatment;
+     public bool[] cutInHalf;
+     public int[] numberOfTimesPerDay;
+     public int[] numberOnHand;
+     public int[] numberInOrder;
+     public int[] dateOfLastOrder;
+     public int[] dateOfLastArrival;
+     public int[] numberOfDaysToReceiveDelivery;
+     public int[] dateOfFirstDoseFromThisOrder;
+ */
 
     public static void main(String[] args) {
         /* TODO Enter code here... */
 
-
-
+    System.out.println("Hello World!");
 
     }
 
