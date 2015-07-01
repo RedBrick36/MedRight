df3dac51ab0255ba8ac84d83b534b3cd5746a2f4
Roger Baran <roger.baran@icloud.com>
2015-07-01 11:26:48 -0500

    New Comments

--- src/main/java/org/redbrick/medright/MedRight.java
+++ src/main/java/org/redbrick/medright/MedRight.java
@@ -6,12 +6,13 @@
 
 package org.redbrick.medright;
 
-
 public class MedRight {
 
 /**
- Believe that what I really need here is an array of arrays and not individual arrays
-/* TODO Find out how to create an array of array's
+ Believe that what I really need here is an array of arrays and not individual arrays.
+
+ As it turns out, what I really need (because the data types differ, is a Collections Framework)
+/* TODO Find & research relevant information for declaring and using the Collections Framework for this application
 
  public string[] treatments;
      public string[] treatmentName;
