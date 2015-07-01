b1fe91c5c6ba6dde59a5ac32e0e25a3128a785fc
Roger Baran <roger.baran@icloud.com>
2015-06-30 21:09:12 -0500

    Setting up beginning of MedRight.java

---
+++ src/main/java/org/redbrick/medright/MedRight.java
@@ -0,0 +1,14 @@
+/**
+ * Created by RedBrick on 6/30/15.
+ *
+ * @author RedBrick
+ */
+package org.redbrick.medright;
+
+public class MedRight {
+
+    public static void main(String[] args) {
+        /* TODO Enter code here... */
+    }
+
+}
