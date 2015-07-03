0d969efdf8b7da3b3078a89b820c0ab384172532
Roger Baran <roger.baran@icloud.com>
2015-07-03 09:06:17 -0500

    Notes & TODO updates

--- src/main/java/org/redbrick/medright/MedRight.java
+++ src/main/java/org/redbrick/medright/MedRight.java
@@ -1,17 +1,22 @@
 /**
- * Created by RedBrick on 6/30/15.
+ * Project started by RedBrick on 6/30/15.
+ *
+ * JDK 1.8.0_45
+ * Groovy 2.4.3
  *
  * @author RedBrick
  */
 
 package org.redbrick.medright;
 
+import javax.swing.*;
+
 public class MedRight {
 
 /**
  Believe that what I really need here is an array of arrays and not individual arrays.
 
- As it turns out, what I really need (because the data types differ, is a Collections Framework)
+ As it turns out, what I really need (because the data types differ), is a Collections Framework (Map?)
 /* TODO Find & research relevant information for declaring and using the Collections Framework for this application
 
  public string[] treatments;
