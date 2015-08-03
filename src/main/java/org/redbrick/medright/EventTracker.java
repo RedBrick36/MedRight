4c7d670b8eb3643bfbbdfe5ed63b298276d01678
Roger Baran <roger.baran@icloud.com>
2015-08-02 19:28:38 -0500

    New class for tracking all event times...

---
+++ src/main/java/org/redbrick/medright/EventTracker.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.redbrick.medright;
+
+/**
+ * Class to keep track of the various schedules created by the user and
+ * dole out the next one in line to the EventTimeSync as and hour and minute
+ * argument set.
+ * 
+ * @author RedBrick
+ */
+public class EventTracker {
+ 
+  
+  
+}
--- src/main/java/org/redbrick/medright/TimerTest.java
+++ src/main/java/org/redbrick/medright/TimerTest.java
@@ -3,10 +3,7 @@ package org.redbrick.medright;
 public class TimerTest {
 
   public static void main(String[] args) throws InterruptedException {
-    
-//    byte[] userCH = new byte[] { 2, 7, 10, 12, 14, 17, 19, 22};
-//    byte[] userCQ = new byte[] {15, 30, 45};
-//    byte qIndex = 1;
+
     
     ClockSync.syncClocks();
     System.out.println("Times are in sync...");
