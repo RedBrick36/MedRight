777479d93c5603d2fb9a1611940c2874c9173890
Roger Baran <roger.baran@icloud.com>
2015-09-02 12:14:37 -0500

    Clean up code

---
+++ target/generated-sources/annotations/treatmentsapp/Treatments_.java
@@ -0,0 +1,39 @@
+package treatmentsapp;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-09-02T12:04:06")
+@StaticMetamodel(Treatments.class)
+public class Treatments_ { 
+
+    public static volatile SingularAttribute<Treatments, Boolean> saturday;
+    public static volatile SingularAttribute<Treatments, Boolean> midam;
+    public static volatile SingularAttribute<Treatments, String> type;
+    public static volatile SingularAttribute<Treatments, Integer> uuid;
+    public static volatile SingularAttribute<Treatments, Boolean> alldays;
+    public static volatile SingularAttribute<Treatments, Boolean> midaft;
+    public static volatile SingularAttribute<Treatments, Boolean> noon;
+    public static volatile SingularAttribute<Treatments, Boolean> asneeded;
+    public static volatile SingularAttribute<Treatments, Boolean> alltimes;
+    public static volatile SingularAttribute<Treatments, Boolean> wednesday;
+    public static volatile SingularAttribute<Treatments, Boolean> friday;
+    public static volatile SingularAttribute<Treatments, Boolean> monday;
+    public static volatile SingularAttribute<Treatments, String> otf;
+    public static volatile SingularAttribute<Treatments, Boolean> reminder;
+    public static volatile SingularAttribute<Treatments, Boolean> thursday;
+    public static volatile SingularAttribute<Treatments, Boolean> am;
+    public static volatile SingularAttribute<Treatments, Boolean> afternoon;
+    public static volatile SingularAttribute<Treatments, Boolean> sunday;
+    public static volatile SingularAttribute<Treatments, String> condition;
+    public static volatile SingularAttribute<Treatments, Double> dose;
+    public static volatile SingularAttribute<Treatments, String> measure;
+    public static volatile SingularAttribute<Treatments, Boolean> tuesday;
+    public static volatile SingularAttribute<Treatments, Boolean> midofnight;
+    public static volatile SingularAttribute<Treatments, String> name;
+    public static volatile SingularAttribute<Treatments, Integer> leadtime;
+    public static volatile SingularAttribute<Treatments, Boolean> evening;
+    public static volatile SingularAttribute<Treatments, Boolean> bedtime;
+
+}
